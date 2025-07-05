c735778b8ec6497cf3cc07f9d2823655e18a905b
WaseemSy <waseemmahamd0@gmail.com>
2025-07-05 22:35:05 +0200

    finished reader.py code

--- library/book.py
+++ library/book.py
@@ -1,31 +1,31 @@
 class Book:
-    
+
     allowed_statuses = ["available", "checked out", "reserved"]
 
-    def __init__(self, title: str, author: str, isbn: str, status: str, year: int):
+    def __init__(self, title: str, author: str, isbn: str, status: str, year: int) -> None:
         self.title = title
         self.author = author
         self.__isbn = isbn
         self.status = status # Using the setter to validate status
         self.year = year
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Title: {self.title}, Author: {self.author}, ISBN: {self.__isbn}, status: {self.status}, Year: {self.year}"
 
-    def __repr__(self):
-        return f"<Book({self.title!r}, {self.author!r}, {self.__isbn!r}, {self.status!r}, {self.year!r})>" 
-    
+    def __repr__(self) -> str:
+        return f"<Book({self.title!r}, {self.author!r}, {self.__isbn!r}, {self.status!r}, {self.year!r})>"
+
 
     @property
-    def isbn(self):
+    def isbn(self) -> str:
         return self.__isbn
     
     @property
-    def status(self):
+    def status(self) -> str:
         return self._status
 
     @status.setter
-    def status(self, status: str):
+    def status(self, status: str) -> None:
         if status not in Book.allowed_statuses:
             raise ValueError(f"Status must be one of {Book.allowed_statuses}")
         else:
---
+++ library/reader.py
@@ -0,0 +1,32 @@
+from user import User
+
+class Reader(User):
+
+    def __init__(self, name: str, user_id: str, borrowed_books : int) -> None:
+        super().__init__(name, user_id)
+        self.__borrowed_books  = borrowed_books 
+
+    def __str__(self)-> str:
+        return f"Name: {self.name}, User ID: {self.user_id}, borrowed_books: {self.borrowed_books}"
+    
+    def __repr__(self)-> str:
+        return f"<Reader(name={self.name!r}, user_id={self.user_id!r}, borrowed_books={self.borrowed_books!r})>"
+
+        
+    # Identify reader as reader role
+    def show_role(self) -> str:
+        return "Reader"
+    
+    @property
+    def borrowed_books(self) -> int:
+        return self.__borrowed_books
+    
+    # Add borrowed books 
+    def borrow_book(self) -> None:
+        self.__borrowed_books +=1
+    
+    # decrease total borrowed books when return a book
+    def return_book(self) -> None:
+        if self.__borrowed_books > 0:
+            self.__borrowed_books -= 1
+    
