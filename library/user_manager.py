f195e70e77dbc627d1da8b7e359ad6baa95e0b19
WaseemSy <waseemmahamd0@gmail.com>
2025-07-06 21:04:09 +0200

    Add comments

--- library/user_manager.py
+++ library/user_manager.py
@@ -11,13 +11,15 @@ class UserManager:
     users (list): represents all users in the system (staff, readers).
     staff_books (dictionary): total books a staff added to the library.
 
-    
-    
-    
-    
+    Methods:
+    add_user(): Add users to a list.
+    get_all_users(): Print all users (staff, readers).
+    get_readers(): Print all readers users.
+    get_staff(): Print all staff users.
+    record_book_addition(): Count how many book a staff added.
+    books_by_staff(): Print total books added by a staff.
+
     """
-
-
 
     def __init__(self) -> None:
         self.__users = []
